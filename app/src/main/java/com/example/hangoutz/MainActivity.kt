9d219f038df37bc89baa9717e43368ee36478b62
MarijaDrobac <drobacmarija3@gmail.com>
2024-11-14 16:30:24 +0100

    User API configuration

--- app/src/main/java/com/example/hangoutz/MainActivity.kt
+++ app/src/main/java/com/example/hangoutz/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 suspend fun getData(){
-    var response = RetrofitInstance.api.getData()
+    var response = RetrofitInstance.api.getUserByName("Android")
     if (response.isSuccessful && response.body() != null) {
         println("odgovor" +response.body())
     } else {
--- app/src/main/java/com/example/hangoutz/data/UserAPI.kt
+++ app/src/main/java/com/example/hangoutz/data/UserAPI.kt
@@ -4,8 +4,10 @@ import com.example.hangoutz.data.models.User
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 
 interface UserAPI {
-    @GET("rest/v1/users?name=eq.Android")
-    suspend fun getData(): Response<List<User>>
+    @GET("rest/v1/users?name=eq.{name}")
+    suspend fun getUserByName(@Path("name") name : String): Response<List<User>>
+
 }
