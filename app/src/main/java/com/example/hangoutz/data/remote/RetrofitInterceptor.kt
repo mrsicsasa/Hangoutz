0c7b6bfb639b2023fc60cd21c1152ab7c0e783ae
SasaMrsic <sasamrsic20@gmail.com>
2024-11-15 11:30:00 +0100

    Moved api key from Constants to apikeys.properties

--- app/src/main/java/com/example/hangoutz/data/remote/RetrofitInterceptor.kt
+++ app/src/main/java/com/example/hangoutz/data/remote/RetrofitInterceptor.kt
@@ -1,6 +1,6 @@
 package com.example.hangoutz.data.remote
 
-import com.example.hangoutz.utils.Constants
+import com.example.hangoutz.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -8,7 +8,7 @@ class RetrofitInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val request = original.newBuilder()
-            .header("apikey", Constants.API_KEY)
+            .header("apikey", BuildConfig.API_KEY)
             .header("Accept", "application/json")
             .method(original.method(), original.body())
             .build()
