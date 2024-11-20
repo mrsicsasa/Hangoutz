ddc31d80435bc6cfab353738cf32d8b39bfccef7
bataneske <vladimirnesic@vivaldi.net>
2024-11-20 16:16:27 +0100

    Register UI and functionality completed

--- app/src/main/java/com/example/hangoutz/ui/theme/Color.kt
+++ app/src/main/java/com/example/hangoutz/ui/theme/Color.kt
@@ -13,4 +13,3 @@ val Chestnut = Color(0xFF4D4444)
 val Error = Color(0xFFFA626C)
 val topBarBackgroundColor = Color(red = 120, green = 137, blue = 149)
 val bottomNavigationColor = Color(red = 43, green = 42, blue = 58)
-
