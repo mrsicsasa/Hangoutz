2120f0a708aea7f4df80d40473f4122613f58b71
MarijaDrobac <drobacmarija3@gmail.com>
2024-11-21 11:42:18 +0100

    Minor bug fix

--- app/src/main/java/com/example/hangoutz/data/local/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/hangoutz/data/local/SharedPreferencesManager.kt
@@ -19,7 +19,7 @@ object SharedPreferencesManager {
     }
 
     fun getUserId(context: Context): String? {
-        SharedPreferencesManager.clearUserId(context)
+        //SharedPreferencesManager.clearUserId(context)
         val sharedPreferences = getSharedPreferences(context)
         return sharedPreferences.getString(KEY_USER_ID, null)
     }
