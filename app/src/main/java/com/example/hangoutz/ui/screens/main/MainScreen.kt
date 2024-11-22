4e54549b9698efd8889647900746ec44d795c80e
SasaMrsic <sasamrsic20@gmail.com>
2024-11-22 10:35:36 +0100

    MBLINTER-6: Added ui for cards and scrollable lazylist

--- app/src/main/java/com/example/hangoutz/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/hangoutz/ui/screens/main/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -25,7 +26,7 @@ import com.example.hangoutz.R
 import com.example.hangoutz.ui.navigation.BottomNavItem
 import com.example.hangoutz.ui.navigation.BottomNavigationDestination
 import com.example.hangoutz.ui.screens.friendsscreen.FriendsScreen
-import com.example.hangoutz.ui.screens.myeventsscreen.MyEventsScreen
+import com.example.hangoutz.ui.screens.myevents.MyEventsScreen
 import com.example.hangoutz.ui.screens.settingsscreen.SettingsScreen
 import com.example.hangoutz.ui.theme.Ivory
 import com.example.hangoutz.ui.theme.topBarBackgroundColor
@@ -43,7 +44,7 @@ fun MainScreen(navController: NavController) {
             title = {
                 Text(
                     text = Constants.TOP_BAR_TITLE,
-                    color = Ivory,
+                    color = Color.White,
                     style = MaterialTheme.typography.titleLarge,
                 )
 
--- app/src/main/java/com/example/hangoutz/ui/theme/Color.kt
+++ app/src/main/java/com/example/hangoutz/ui/theme/Color.kt
@@ -2,14 +2,17 @@ package com.example.hangoutz.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Ivory = Color(0xFFE2D7D3)
-val Orange = Color(0xFFDDAE74)
-val PurpleDark = Color(0xFF765A71)
-val GreenMinty = Color(0xFFACC199)
-val GreenDark = Color(0xFF618181)
-val Blue = Color(0xFF848AAB)
-val OrangeDark = Color(0xFFAF9767)
+val Ivory = Color(0xFFe2d7d3)
+val Orange = Color(0xCCDDAE74)
+val PurpleDark = Color(0xCC765A71)
+val GreenMinty = Color(0xFFacc199)
+val GreenDark = Color(0xCC618181)
+val Blue = Color(0xFF848aab)
+val OrangeDark = Color(0xFFaf9767)
 val Chestnut = Color(0xFF4D4444)
 val Error = Color(0xFFFA626C)
-val topBarBackgroundColor = Color(0xFF788995)
-val bottomNavigationColor = Color(0xFF2B2A3A)
+val topBarBackgroundColor = Color(0xCC788995)
+val bottomNavigationColor = Color(red = 43, green = 42, blue = 58)
+val textBodyGrayColor = Color(red = 217, green = 217, blue = 217)
+val ShadowColor = Color(0xFF686C85)
+
