a0bcce90ef5d5029296daff393554dfa2756aea5
SasaMrsic <sasamrsic20@gmail.com>
2024-11-22 12:08:31 +0100

    MBLINTER-6: getNumberOfUsersAccepted

--- app/src/main/java/com/example/hangoutz/ui/screens/myevents/EventScreenState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.hangoutz.ui.screens.myevents
-
-import com.example.hangoutz.data.models.EventCardDPO
-
-data class EventScreenState(
-    var events: List<EventCardDPO> = emptyList()
-)
